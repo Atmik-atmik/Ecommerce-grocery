56b0098b2618e5d314d213bc93021a5127275491
Atmik Dubey <atmikd6@gmail.com>
2025-11-15 19:28:49 +0530

    feat: enhance Category component with Swiper for shop collections display

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -24,6 +24,37 @@ const Category = () => {
         <h2 className="max-w-lg text-lg font-medium">Category</h2>
         <div className="ml-1 w-20 flex border-b border-[var(--color-secondary)] border-2 "></div>
       </div>
+      <h2 className="mt-4 text-[var(--color-secondary)] font-extrabold text-3xl ">
+        Shop By Collection{" "}
+      </h2>
+
+      <Swiper
+        modules={{ Autoplay }}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        loop={true}
+        slidesPerview={6}
+        breakpoints={{
+          0: {
+            slidesPerview: 2,
+          },
+
+          768: {
+            slidesPerview: 3,
+          },
+          1024: {
+            slidesPerview: 6,
+          },
+        }}
+        className="w-full my-5"
+      >
+        {categoriesData.map((category, i) => (
+          <SwiperSlide key={i}>
+            <motion.div>
+              <img src={category.image} alt="" className="w-32 h-32" />
+            </motion.div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
