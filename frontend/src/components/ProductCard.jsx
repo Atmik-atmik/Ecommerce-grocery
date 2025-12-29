9d797a8db19420a35aea89ffb2fc23d4b59e5cf0
Atmik Dubey <atmikd6@gmail.com>
2025-12-29 21:04:26 +0530

    feat: enhance ProductCard layout with additional product details

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -13,6 +13,21 @@ const ProductCard = ({ product }) => {
       <button className="flex items-center justify-center mb-3 w-full py-1 bg-[var(--color-secondary)] text-white cursor-pointer ">
         <ShoppingCart />
       </button>
+      <hr className="w-full" />
+      <div>
+        <p className="text-[var(--color-secondary)] text-sm font-normal ">
+          {product.category}{" "}
+        </p>
+        <h2 className="text-lg font-semibold text-gray-800 ">
+          {product.name}{" "}
+        </h2>
+      </div>
+      <div className="flex items-center gap-4">
+        <p className="text-base font-normal line-through text-gray-400">
+          {product.price}
+        </p>
+        <p className="text-base font-normal">{product.offerPrice} </p>
+      </div>
     </div>
   );
 };
